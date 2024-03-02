e0dd047943b1d2202d8850b98397698974dbcfab
Asharib Ali <asharibarain4@gmail.com>
2024-03-02 09:08:27 +0500

    add day-04

---
+++ day-04/variables.js
@@ -0,0 +1,7 @@
+var message = 'Hello, TypeScript!';
+var year = 2021;
+var isLearningFun = true;
+// you can print the variables to the console via console.log
+console.log(message); // Hello, TypeScript!
+console.log(year); // 2021
+console.log(isLearningFun); // true
---
+++ day-04/variables.ts
@@ -0,0 +1,8 @@
+let message: string = 'Hello, TypeScript!'; 
+let year: number = 2021; 
+let isLearningFun: boolean = true;
+
+// you can print the variables to the console via console.log
+console.log(message); // Hello, TypeScript!
+console.log(year); // 2021
+console.log(isLearningFun); // true
