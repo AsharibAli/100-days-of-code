b852e7405d21283be994d209f03b1dfbef3dcdc3
Ahmed Yar Khan <159126492+Tribalschief@users.noreply.github.com>
2024-03-19 23:31:54 +0500

    Update main.ts

--- day-18/q54/main.ts
+++ day-18/q54/main.ts
@@ -1,6 +1,6 @@
 // A way to make a flexible list
 function createObjectWithDynamicKey(key: string, value: string) {
-  let dynamicObject = {};
+  let dynamicObject : {[key:string] : string} = {}; // by placing {} we get implicity error we can fix this by put value of string in array which is key and string which is value
   // Setting up a section in the list with a changeable name
   dynamicObject[key] = value;
   return dynamicObject;
